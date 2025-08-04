bc92e9409da0eecc51a1c66edf96c823e0f1fb0d
alex <alex@pdp7.net>
2025-08-04 20:08:47 +0200

    Flesh out

--- src/frozenplanetoid/__init__.py
+++ src/frozenplanetoid/__init__.py
@@ -1,8 +1,32 @@
 import argparse
+import dataclasses
 import pathlib
+import pprint
 import yaml
 
 import feedparser
+
+
+class Feed:
+    def __init__(self, d):
+        self.d = d
+
+    @property
+    def rss(self):
+        return self.d["rss"]
+
+    def load(self):
+        # self.parsed = feedparser.parse(self.rss)
+        pass
+
+    def __repr__(self):
+        return f"Feed({self.d['rss']})"
+
+
+@dataclasses.dataclass
+class Category:
+    children_categories: dict[str, "Categories"]
+    feeds: list[Feed]
 
 
 def main():
@@ -11,5 +35,18 @@ def main():
     args = parser.parse_args()
     config = yaml.load(args.config.read_text(), yaml.SafeLoader)
     feeds = config
+
+    root_category = Category(children_categories={}, feeds=[])
+
     for feed in feeds:
-        feed = feedparser.parse(feed["rss"])
+        feed = Feed(feed)
+        feed.load()
+
+        for feed_category in feed.d["categories"]:
+            category_pointer = root_category
+            for feed_category_path in feed_category.split("/"):
+                if feed_category_path not in category_pointer.children_categories:
+                    category_pointer.children_categories[feed_category_path] = Category(children_categories={}, feeds=[])
+                category_pointer = category_pointer.children_categories[feed_category_path]
+                category_pointer.feeds.append(feed)
+    pprint.pprint(root_category)
